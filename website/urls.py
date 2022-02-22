d61b8e330d60eb3098d04cbc6dc77c6f76bc6018
montalvas <lucas_montalvani@hotmail.com>
2022-02-21 21:03:15 -0300

    Organizando os templates

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AboutView
 
 app_name = 'website'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('about/', AboutView.as_view(), name='about')
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
-from re import template
-from xml.dom.expatbuilder import TEXT_NODE
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = "website/index.html"
+    
+class AboutView(TemplateView):
+    template_name = "website/about.html"
+    
+
