0473fcbca723948bc515d5fcbeb64492e894bb5b
montalvas <lucas_montalvani@hotmail.com>
2022-03-04 22:39:48 -0300

    Add urls, views & templates for new models

--- records/urls.py
+++ records/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import FieldCreate, ActivityCreate
-from .views import FieldUpdate, ActivityUpdate
-from .views import FieldDelete, ActivityDelete
-from .views import FieldList, ActivityList
+
+from .views import FieldCreate, ActivityCreate, StateCreate, CityCreate, CampusCreate, OfficialCreate, StatusCreate, SituationCreate, GradeCreate, ProgressionCreate, ReceiptCreate, ValidationCreate
+
+from .views import FieldUpdate, ActivityUpdate, StateUpdate, CityUpdate, CampusUpdate, OfficialUpdate, StatusUpdate, SituationUpdate, GradeUpdate, ProgressionUpdate, ReceiptUpdate, ValidationUpdate
+
+from .views import FieldDelete, ActivityDelete, StateDelete, CityDelete, CampusDelete, OfficialDelete, StatusDelete, SituationDelete, GradeDelete, ProgressionDelete, ReceiptDelete, ValidationDelete
+
+from .views import FieldList, ActivityList, StateList, CityList, CampusList, OfficialList, StatusList, SituationList, GradeList, ProgressionList, ReceiptList, ValidationList
 
 app_name = 'records'
 
@@ -10,17 +14,57 @@ urlpatterns = [
     # register
     path('register/field/', FieldCreate.as_view(), name='register-field'),
     path('register/activity/', ActivityCreate.as_view(), name='register-activity'),
+    path('register/state/', StateCreate.as_view(), name='register-state'),
+    path('register/city/', CityCreate.as_view(), name='register-city'),
+    path('register/campus/', CampusCreate.as_view(), name='register-campus'),
+    path('register/official/', OfficialCreate.as_view(), name='register-official'),
+    path('register/status/', StatusCreate.as_view(), name='register-status'),
+    path('register/situation/', SituationCreate.as_view(), name='register-situation'),
+    path('register/grade/', GradeCreate.as_view(), name='register-grade'),
+    path('register/progression/', ProgressionCreate.as_view(), name='register-progression'),
+    path('register/receipt/', ReceiptCreate.as_view(), name='register-receipt'),
+    path('register/validation/', ValidationCreate.as_view(), name='register-validation'),
     
     # update
     path('update/field/<int:pk>', FieldUpdate.as_view(), name='update-field'),
     path('update/activity/<int:pk>', ActivityUpdate.as_view(), name='update-activity'),
+    path('update/state/<int:pk>', StateUpdate.as_view(), name='update-state'),
+    path('update/city/<int:pk>', CityUpdate.as_view(), name='update-city'),
+    path('update/campus/<int:pk>', CampusUpdate.as_view(), name='update-campus'),
+    path('update/official/<int:pk>', OfficialUpdate.as_view(), name='update-official'),
+    path('update/status/<int:pk>', StatusUpdate.as_view(), name='update-status'),
+    path('update/situation/<int:pk>', SituationUpdate.as_view(), name='update-situation'),
+    path('update/grade/<int:pk>', GradeUpdate.as_view(), name='update-grade'),
+    path('update/progression/<int:pk>', ProgressionUpdate.as_view(), name='update-progression'),
+    path('update/receipt/<int:pk>', ReceiptUpdate.as_view(), name='update-receipt'),
+    path('update/validation/<int:pk>', ValidationUpdate.as_view(), name='update-validation'),
     
     # delete
     path('delete/field/<int:pk>', FieldDelete.as_view(), name='delete-field'),
     path('delete/activity/<int:pk>', ActivityDelete.as_view(), name='delete-activity'),
+    path('delete/state/<int:pk>', StateDelete.as_view(), name='delete-state'),
+    path('delete/city/<int:pk>', CityDelete.as_view(), name='delete-city'),
+    path('delete/campus/<int:pk>', CampusDelete.as_view(), name='delete-campus'),
+    path('delete/official/<int:pk>', OfficialDelete.as_view(), name='delete-official'),
+    path('delete/status/<int:pk>', StatusDelete.as_view(), name='delete-status'),
+    path('delete/situation/<int:pk>', SituationDelete.as_view(), name='delete-situation'),
+    path('delete/grade/<int:pk>', GradeDelete.as_view(), name='delete-grade'),
+    path('delete/progression/<int:pk>', ProgressionDelete.as_view(), name='delete-progression'),
+    path('delete/receipt/<int:pk>', ReceiptDelete.as_view(), name='delete-receipt'),
+    path('delete/validation/<int:pk>', ValidationDelete.as_view(), name='delete-validation'),
+    
     
     # lista
     path('list/field', FieldList.as_view(), name='list-field'),
     path('list/activity', ActivityList.as_view(), name='list-activity'),
-    
+    path('list/state', StateList.as_view(), name='list-state'),
+    path('list/city', CityList.as_view(), name='list-city'),
+    path('list/campus', CampusList.as_view(), name='list-campus'),
+    path('list/official', OfficialList.as_view(), name='list-official'),
+    path('list/status', StatusList.as_view(), name='list-status'),
+    path('list/situation', SituationList.as_view(), name='list-situation'),
+    path('list/grade', GradeList.as_view(), name='list-grade'),
+    path('list/progression', ProgressionList.as_view(), name='list-progression'),
+    path('list/receipt', ReceiptList.as_view(), name='list-receipt'),
+    path('list/validation', ValidationList.as_view(), name='list-validation'),
 ]
--- records/views.py
+++ records/views.py
@@ -4,7 +4,8 @@ from django.views.generic import DeleteView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from braces.views import GroupRequiredMixin
 
-from .models import Field, Activity
+from .models import Field, Activity, Official, State, City, Campus, Status, Validation
+from .models import Situation, Grade, Progression, Receipt
 
 from django.urls import reverse_lazy
 
@@ -18,44 +19,250 @@ class FieldCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
     template_name = 'records/form.html'
     success_url = reverse_lazy('records:list-field')
     
-class ActivityCreate(GroupRequiredMixin ,LoginRequiredMixin, CreateView):
+class ActivityCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
     group_required = u"Administrador" 
     model = Activity
     fields = ['number', 'description', 'points', 'details', 'field']
     template_name = 'records/form.html'
     success_url = reverse_lazy('records:list-activity')
     
+class StateCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = State
+    fields = ['name', 'uf']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-state')
+    
+class CityCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = City
+    fields = ['name', 'state']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-city')
+    
+class CampusCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Campus
+    fields = ['name', 'city', 'address', 'phone']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-campus')
+    
+class OfficialCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Official
+    fields = ['full_name', 'siape', 'cpf', 'campus']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-official')
+    
+class StatusCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Status
+    fields = ['name', 'description']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-status')
+
+class SituationCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Situation
+    fields = ['status', 'details']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-situation')
+    
+class GradeCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Grade
+    fields = ['name', 'level', 'description']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-grade')
+    
+class ProgressionCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Progression
+    fields = ['grade', 'initial_date', 'final_date', 'observation']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+class ReceiptCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Receipt
+    fields = ['progression', 'activity', 'quantity', 'initial_date', 'final_date', 'file']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+class ValidationCreate(GroupRequiredMixin, LoginRequiredMixin, CreateView):
+    group_required = u"Administrador" 
+    model = Validation
+    fields = ['quantity', 'justification']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+
 ################ UPDATE ################
 
-class FieldUpdate(GroupRequiredMixin ,LoginRequiredMixin, UpdateView):
+
+class FieldUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
     group_required = u"Administrador" 
     model = Field
     fields = ['name', 'description']
     template_name = 'records/form.html'
     success_url = reverse_lazy('records:list-field')
     
-class ActivityUpdate(GroupRequiredMixin ,LoginRequiredMixin, UpdateView):
+class ActivityUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
     group_required = u"Administrador" 
     model = Activity
     fields = ['number', 'description', 'points', 'details', 'field']
     template_name = 'records/form.html'
     success_url = reverse_lazy('records:list-activity')
     
+class StateUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = State
+    fields = ['name', 'uf']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-state')
+    
+class CityUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = City
+    fields = ['name', 'state']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-city')
+    
+class CampusUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Campus
+    fields = ['name', 'city', 'address', 'phone']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-campus')
+    
+class OfficialUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Official
+    fields = ['full_name', 'siape', 'cpf', 'campus']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-official')
+    
+class StatusUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Status
+    fields = ['name', 'description']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-status')
+
+class SituationUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Situation
+    fields = ['status', 'details']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-situation')
+    
+class GradeUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Grade
+    fields = ['name', 'level', 'description']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-grade')
+    
+class ProgressionUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Progression
+    fields = ['grade', 'initial_date', 'final_date', 'observation']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+class ReceiptUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Receipt
+    fields = ['progression', 'activity', 'quantity', 'initial_date', 'final_date', 'file']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+class ValidationUpdate(GroupRequiredMixin, LoginRequiredMixin, UpdateView):
+    group_required = u"Administrador" 
+    model = Validation
+    fields = ['quantity', 'justification']
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+
 ################ DELETE ################
 
-class FieldDelete(GroupRequiredMixin ,LoginRequiredMixin, DeleteView):
+
+class FieldDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
     group_required = u"Administrador" 
     model = Field 
     template_name = 'records/form-delete.html'
     success_url = reverse_lazy('records:list-field')
     
-class ActivityDelete(GroupRequiredMixin ,LoginRequiredMixin, DeleteView):
+class ActivityDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
     group_required = u"Administrador" 
     model = Activity
     template_name = 'records/form-delete.html'
     success_url = reverse_lazy('records:list-activity')
+    
+class StateDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = State
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-state')
+    
+class CityDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = City
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-city')
+    
+class CampusDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Campus
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-campus')
+    
+class OfficialDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Official
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-official')
+    
+class StatusDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Status
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-status')
+
+class SituationDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Situation
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-situation')
+    
+class GradeDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Grade
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-grade')
+    
+class ProgressionDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Progression
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+class ReceiptDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Receipt
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+    
+class ValidationDelete(GroupRequiredMixin, LoginRequiredMixin, DeleteView):
+    group_required = u"Administrador" 
+    model = Validation
+    template_name = 'records/form.html'
+    success_url = reverse_lazy('records:list-progression')
+
 
 ################ LIST ################
+
 
 class FieldList(LoginRequiredMixin, ListView):
     model = Field
@@ -66,4 +273,54 @@ class ActivityList(LoginRequiredMixin, ListView):
     model = Activity
     template_name = 'records/lists/activity.html'
     context_object_name = 'activities'
+    
+class StateList(LoginRequiredMixin, ListView):
+    model = State
+    template_name = 'records/lists/state.html'
+    context_object_name = 'states'
+    
+class CityList(LoginRequiredMixin, ListView): 
+    model = City
+    template_name = 'records/lists/city.html'
+    context_object_name = 'cities'
+    
+class CampusList(LoginRequiredMixin, ListView): 
+    model = Campus
+    template_name = 'records/lists/campus.html'
+    context_object_name = 'campus'
+    
+class OfficialList(LoginRequiredMixin, ListView): 
+    model = Campus
+    template_name = 'records/lists/official.html'
+    context_object_name = 'officials'
+    
+class StatusList(LoginRequiredMixin, ListView):
+    model = Status
+    template_name = 'records/lists/status.html'
+    context_object_name = 'status'
+
+class SituationList(LoginRequiredMixin, ListView):
+    model = Situation
+    template_name = 'records/lists/situation.html'
+    context_object_name = 'situations'
+    
+class GradeList(LoginRequiredMixin, ListView):
+    model = Grade
+    template_name = 'records/lists/grade.html'
+    context_object_name = 'grades'
+    
+class ProgressionList(LoginRequiredMixin, ListView):
+    model = Progression
+    template_name = 'records/lists/progression.html'
+    context_object_name = 'progressions'
+    
+class ReceiptList(LoginRequiredMixin, ListView):
+    model = Receipt
+    template_name = 'records/lists/receipt.html'
+    context_object_name = 'receipts'
+    
+class ValidationList(LoginRequiredMixin, ListView):
+    model = Validation
+    template_name = 'records/lists/validation.html'
+    context_object_name = 'validations'
 
