4f4fccba2eab3a4dc9df0582686159c4a73b2548
montalvas <lucas_montalvani@hotmail.com>
2022-03-09 07:28:34 -0300

    email validation and group user

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from enum import unique
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class UserForm(UserCreationForm):
@@ -11,4 +12,10 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+    def clean_email(self):
+        e = self.cleaned_data['email']
+        
+        if User.objects.filter(email=e).exists():
+            raise ValidationError('O e-mail já está cadastrado.')
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
 from django.views.generic import CreateView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+
 from .forms import UserForm
 
 
@@ -8,4 +11,11 @@ class UserCreate(CreateView):
     template_name = 'users/register.html'
     model = User
     form_class = UserForm
-    success_url = reverse_lazy('website:index')
+    success_url = reverse_lazy('auth:login')
+    
+    def form_valid(self, form):
+        group = get_object_or_404(Group, name='Docente')
+        url = super().form_valid(form)
+        self.object.groups.add(group)
+        
+        return url
